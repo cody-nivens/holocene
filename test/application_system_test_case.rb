c5e95ffe145d809f6547fc0bb2ee361342e5814e
Cody Nivens <cody.nivens@gmail.com>
2018-11-21 08:31:55 -0800

    Added code for headless browser in system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,20 +1,39 @@
 require "test_helper"
 
-class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-    # https://svn.redmine.org/redmine/trunk/test/application_system_test_case.rb
-    #
-  DOWNLOADS_PATH = File.expand_path(File.join(Rails.root, 'tmp', 'downloads'))
+DOWNLOADS_PATH = File.expand_path(File.join(Rails.root, 'tmp', 'downloads'))
+Capybara.register_driver :headless_selenium do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
 
-  driven_by :selenium, using: :chrome, screen_size: [1024, 900], options: {
-      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-        'chromeOptions' => {
-          'prefs' => {
-            'download.default_directory' => DOWNLOADS_PATH,
-            'download.prompt_for_download' => false,
-            'plugins.plugins_disabled' => ["Chrome PDF Viewer"]
-          }
-        }
-      )
-    }
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--disable-popup-blocking')
+  options.add_argument('--window-size=1366,768')
+
+  options.add_preference(:download, directory_upgrade: true,
+                                    prompt_for_download: false,
+                                    default_directory: DOWNLOADS_PATH)
+
+  options.add_preference(:browser, set_download_behavior: { behavior: 'allow' })
+
+  driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+
+  bridge = driver.browser.send(:bridge)
+
+  path = '/session/:session_id/chromium/send_command'
+  path[':session_id'] = bridge.session_id
+
+  bridge.http.call(:post, path, cmd: 'Page.setDownloadBehavior',
+                                params: {
+                                  behavior: 'allow',
+                                  downloadPath: DOWNLOADS_PATH
+                                })
+
+  driver
+end
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+
+  driven_by :headless_selenium
 
 end
