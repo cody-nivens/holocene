76df5e196e3c6db363c7dc5f2d8f10724492caa9
Cody Nivens <cody.nivens@gmail.com>
2019-12-10 11:04:34 -0800

    Added holocene events to create counts of what happened in an Epoch

--- app/models/epoch.rb
+++ app/models/epoch.rb
@@ -13,4 +13,8 @@ class Epoch < ApplicationRecord
     def map_locs
         return HoloceneEvent.where(["start_year >= ? and start_year <= ?", self.start_date,self.end_date]).collect{|x| (x.lat.nil? ? nil : x.location) }.compact
     end
+
+    def holocene_events
+        return HoloceneEvent.where(["start_year >= ? and start_year <= ?", self.start_date,self.end_date])
+    end
 end
