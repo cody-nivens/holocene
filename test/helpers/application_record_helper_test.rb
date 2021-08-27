31ac8237f8bdf1bdc5cfaa8ba863727e8fea5210
Cody Nivens <cody.nivens@gmail.com>
2021-08-27 10:13:03 -0700

    Tests for helpers

--- test/helpers/application_record_helper_test.rb
+++ test/helpers/application_record_helper_test.rb
@@ -1,5 +1,15 @@
 require 'test_helper'
 
 class ApplicationRecordsHelperTest < ActionView::TestCase
-  # ...
+  test "supress the first crumb" do
+    character = characters(:character_1)
+    values = character.set_values
+
+    string = breadcrumb(values[0], values[1], values[2], values[3], values[4])
+
+    string2 =surpress_first(string)
+
+    assert_not_equal string, string2
+  end
+
 end
--- test/helpers/scene_helper_test.rb
+++ test/helpers/scene_helper_test.rb
@@ -1,5 +1,9 @@
 require 'test_helper'
 
 class ScenesHelperTest < ActionView::TestCase
-  # ...
+  test "scene should know book" do
+    scene = scenes(:scene_1)
+    book = books(:book_2)
+    assert_equal book,scene.book
+  end
 end
