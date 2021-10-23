477f28e1d2cb78d34e0a32451ebc16dbcf744001
Cody Nivens <cody.nivens@gmail.com>
2021-10-22 22:47:18 -0700

    Another bit

--- db/migrate/20210725133834_migrate_book_body_to_action_text.rb
+++ db/migrate/20210725133834_migrate_book_body_to_action_text.rb
@@ -7,9 +7,9 @@ class MigrateBookBodyToActionText < ActiveRecord::Migration[6.0]
     #end
     #remove_column :books, :body_old
     #rename_column :books, :publisher, :publisher_old
-    Book.all.each do |book|
-      book.update_attribute(:publisher, simple_format(book.publisher_old, {}, wrapper_tag: "div"))
-    end
-    remove_column :books, :publisher_old
+    #Book.all.each do |book|
+    #  book.update_attribute(:publisher, simple_format(book.publisher_old, {}, wrapper_tag: "div"))
+    #end
+    #remove_column :books, :publisher_old
   end
 end
