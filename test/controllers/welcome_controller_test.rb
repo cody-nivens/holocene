a60931d1c555f32bb813f3a16f688b9667dad005
Cody Nivens <cody.nivens@gmail.com>
2022-08-18 07:27:26 -0700

    Updated gems

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,12 +4,18 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @section = sections(:section_1)
     @user = users(:users_1)
     sign_in @user
   end
 
   test 'should get index' do
     get welcome_index_url
+    assert_response :success
+  end
+
+  test 'should get show' do
+    get welcome_show_url(@section,date: Date.today.beginning_of_day)
     assert_response :success
   end
 
